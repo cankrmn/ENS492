6b2cde9b7b3b87940a5d101f686cd91987df0514
bengisu <bengisueren@sabanciuniv.edu>
2022-11-02 21:26:29 +0300

    only raw text

--- Src/security_intelligence.py
+++ Src/security_intelligence.py
@@ -1,41 +1,28 @@
-from datetime import datetime
+from modulefinder import packagePathMap
 from bs4 import BeautifulSoup
 import requests
-import pandas as pd
-# from modulefinder import packagePathMap
+import string
 
-from Utils.header import header
-from Utils.format_text import formatText
 
-def tokenizeSecurityIntelligence(url):
-  data = requests.get(url, headers = header).text
-  new_soup = BeautifulSoup(data, "lxml")
-  #body = new_soup.find_all("div", itemprop = "articleBody")
-
-  body=""
-  for div in new_soup.find_all('main', class_='post__content'):
-    for p in div.find_all(['h2', 'p']):
-      if p.parent["class"][0] != "author__description":
-        body = body + " " + (p.text) 
-  return body
+header = {
+  "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36"
+}
 
 def scrapeSecurityIntelligence(url):
   html_text = requests.get(url, headers = header).text
   soup = BeautifulSoup(html_text, "lxml")
 
-  dic = {} 
-  dic["title"] = soup.find("p", class_= "breadcrumbs__page_title").text
-  #dic["first paragraph"] = soup.find("main", class_="post__content").find('p').text
-  dic["url"] = url
+  dic = {}  
 
-  date_string = soup.find("span", class_="article__info__date").text.replace(',', '').replace(' ', '-')
-  dic["date"] = str(datetime.strptime(date_string, '%B-%d-%Y').date()) if len(date_string)!= "" else "No Date"   
+  body=""
+  for div in soup.find_all('main', class_='post__content'):
+    for p in div.find_all(['h2', 'p']):
+      if p.parent["class"][0] != "author__description":
+        body = body + " " + (p.text) 
 
-  rawText = tokenizeSecurityIntelligence(url)
-  dic["raw text"] = rawText
-  dic["stemmed text"] = formatText(rawText)
-  #print(dic)
+  dic["raw text"] = body
+  
   return dic
 
 
-# scrapeSecurityIntelligence("https://securityintelligence.com/articles/sec-business-data-breach/")
+#scrapeSecurityIntelligence("https://securityintelligence.com/articles/sec-business-data-breach/")
--- Src/threat_post.py
+++ Src/threat_post.py
@@ -1,94 +1,22 @@
-from datetime import datetime
 from modulefinder import packagePathMap
 from bs4 import BeautifulSoup
 import requests
 import string
-import pandas as pd
-import nltk
-nltk.download('punkt')
-nltk.download('stopwords')
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-
-from nltk.tokenize import sent_tokenize, word_tokenize
-
-from nltk.stem.snowball import SnowballStemmer
-englishStemmer=SnowballStemmer("english")
-
-nltk.download('wordnet')
-nltk.download('omw-1.4')
-from nltk.stem import WordNetLemmatizer
-wordnet_lemmatizer = WordNetLemmatizer()
 
 
 header = {
   "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36"
 }
-def removeStopWords(sentence):
-  stop_words = set(stopwords.words('english'))
-  word_tokens = word_tokenize(sentence)
-  filtered_sentence = [w.lower() for w in word_tokens if not w.lower() in stop_words]
 
-  # filtered_sentence to single string
-  newSentence = ""
-
-  for word in filtered_sentence:
-    newSentence += word + " "
-
-  newSentence = newSentence.translate(newSentence.maketrans("", "", string.punctuation + "“”’0123456789"))
-
-  return newSentence
-
-
-def stemSentence(sentence):
-  token_words=word_tokenize(sentence)
-  stem_sentence=[]
-  for word in token_words:
-    w = (wordnet_lemmatizer.lemmatize(word, pos="n"))
-    stem_sentence.append(englishStemmer.stem(w))
-    stem_sentence.append(" ")
-  return "".join(stem_sentence)
-
-def tokenizeThreatPost(url):
-  data = requests.get(url, headers = header).text
-  new_soup = BeautifulSoup(data, "lxml")
-  #body = new_soup.find_all("div", itemprop = "articleBody")
-
-  body=""
-  for div in new_soup.find_all('div', class_='c-article__content js-reading-content'):
-    for p in div.find_all(['p','h2'])[:-1]:
-        body = body + (p.text)
-  return body
-'''''
-  newsText = ""
-
-  for txt in body:
-    newsText += txt.strip()
-  
-  newsText = newsText.translate(newsText.maketrans("", "", string.punctuation + "“”0123456789"))
-
-  sentence = removeStopWords(newsText)
-  sentence = stemSentence(sentence)
-  return sentence
-'''
-  # return sentence.split(" ")
-  # return tokenize([sentence])
-
-
-def scrapeThreatPost(url):
+def scrapeTheHackerNews(url):
   html_text = requests.get(url, headers = header).text
   soup = BeautifulSoup(html_text, "lxml")
-
   dic = {} 
-  dic["title"] = soup.find("h1", class_= "c-article__title").text
-  #dic["first paragraph"] = soup.find("div", class_="c-article__intro").text
-  dic["url"] = url
+  body=""
+  for div in soup.find_all('div', id='articlebody'):
+    for p in div.find_all('p'):
+        body = body + (p.text)
+  dic["raw text"] = body
+  return dic
 
-  date_string = soup.find("div", class_="c-article__time").find("time").get("datetime").split('T')[0]
-  dic["date"] = date_string if len(date_string)!= "" else "No Date"   
-
-  dic["stemmed text"] = tokenizeThreatPost(url)
-  #print(dic)
-
-
-scrapeThreatPost("https://threatpost.com/cisco-network-breach-google/180385/")
+#scrapeTheHackerNews("https://thehackernews.com/2022/10/russian-hacker-arrested-in-india-for.html")
