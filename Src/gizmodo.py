7860a2b7c9f143f5189ba187ff719928509a759b
keremkz <94916101+Orekk12@users.noreply.github.com>
2022-11-03 02:26:11 +0300

    Create gizmodo.py

---
+++ Src/gizmodo.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup
+import requests
+import string
+import re
+
+header = {
+  "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36"
+}
+
+url1 = "https://gizmodo.com/china-mysterious-spaceplane-ejects-unknown-object-1849733129"
+
+def scrapeRegister(url):
+  html_text = requests.get(url, headers=header).text
+  soup = BeautifulSoup(html_text, "lxml")
+  dic = {}
+  textContainer = soup.find("div", class_="xs32fe-0 hmTkUM js_post-content")
+  pArray = textContainer.find_all("p")
+  rawText = getText(pArray, ['<span>(.*?)">', '<\/a><\/span>', '<p.*?">', '<\/p>,', '<.*?>'])
+  dic["raw text"] = rawText
+  #dic["stemmed text"] = formatText(rawText)
+  #print(dic["raw text"])
+  return dic
+
+def getText(text, regexList):
+  newText = text
+  for regex in regexList:
+    newText = re.sub(regex, '', str(newText))
+  return newText
