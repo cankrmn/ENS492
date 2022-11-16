5fddcfd8049161b6e6ca0a097be0db76cb0023a0
idilkapikiran <ikapikiran@sabanciuniv.edu>
2022-11-16 14:28:04 +0300

    updated crawlers to discard empty data

--- Src/vice.py
+++ Src/vice.py
@@ -12,25 +12,12 @@ def scrapeVice(url):
     html_text = requests.get(url, headers=header).text
     soup = BeautifulSoup(html_text, "lxml")
     dic = {}
-    textContainer = soup.find("div",
-                              class_="article__body-components")
-    pArray = textContainer.find_all("p")
-    pArray.pop()
-    rawText = " ".join(list(map(getText, pArray)))
-    dic["raw text"] = rawText
-    print(dic)
+    if soup is not None:
+        textContainer = soup.find("div",
+                                  class_="article__body-components")
+        if textContainer is not None:
+            pArray = textContainer.find_all("p")
+            pArray.pop()
+            rawText = " ".join(list(map(getText, pArray)))
+            dic["raw text"] = rawText
     return dic
-
-
-# will be deleted later
-jsonFile = open("../packet_storm.json", "r+")
-packet_storm = json.load(jsonFile)
-vice_news = packet_storm["VICE"]
-scrapeVice(
-    'https://www.vice.com/en/article/4axqed/cybercriminals-leak-la-school-data-after-it-refuses-to-ransom')
-
-jsonFile.seek(0)
-# convert back to json.
-json.dump(packet_storm, jsonFile, indent=2)
-
-jsonFile.close()
